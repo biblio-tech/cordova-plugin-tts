e7ca25caaa5a5440efb2aa96aded39ab8e77a1aa
Ryan Schingeck <ryan.schingeck@bibliotech.com>
2020-10-06 10:23:28 -0400

    add import

--- src/android/TTS.java
+++ src/android/TTS.java
@@ -14,6 +14,7 @@ import android.os.Build;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
 import android.speech.tts.UtteranceProgressListener;
+import android.speech.tts.Voice;
 
 import java.util.HashMap;
 import java.util.Locale;
@@ -221,6 +222,7 @@ public class TTS extends CordovaPlugin implements OnInitListener {
         }
 
         String voices = "";
+
         for (Voice tmpVoice : tts.getVoices()) {
             voices = voices + "," + tmpVoice.getName();
         }
